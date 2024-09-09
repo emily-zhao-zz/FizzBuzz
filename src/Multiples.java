26cf85bef6e8dc5c9bef3d7ab5cdbe81e78a91e0
Emily <emily@s-MacBook-Air-4.local>
2024-09-09 16:32:40 -0400

    create

---
+++ src/Multiples.java
@@ -0,0 +1,13 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int num = 0;
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+            if (divisibleBy3 | divisibleBy5) {
+                num ++;
+            }
+        }
+        System.out.println(num);
+    }
+}
